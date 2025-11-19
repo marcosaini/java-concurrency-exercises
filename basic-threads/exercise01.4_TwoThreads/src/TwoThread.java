c72b815d8ff13eb43efefbba77603e51891c767f
Marco Saini <marcosaini@protonmail.com>
2025-11-19 09:40:23 +0100

    Update TwoThread.java

--- basic-threads/exercise01.4_TwoThreads/src/TwoThread.java
+++ basic-threads/exercise01.4_TwoThreads/src/TwoThread.java
@@ -1,6 +1,7 @@
 class WorkerThread extends Thread {
+    static int counter = 0;
     public void run() {
-        System.out.println("Worker thread is running.");
+        System.out.println("Worker thread is running--" + counter++);
     }
 }   
 
@@ -10,9 +11,22 @@ class WorkerRunnable implements Runnable {
     }
 }
 public class TwoThread {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Thread thread1 = new WorkerThread();
         Thread thread2 = new Thread(new WorkerRunnable());
+        Thread[] threads = new WorkerThread[10];
+        for (int i = 0; i < 10; i++) {
+            threads[i] = new WorkerThread();
+            threads[i].start();
+        }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
         thread1.start();
         thread2.start();
         System.out.println("Hello, World!");
